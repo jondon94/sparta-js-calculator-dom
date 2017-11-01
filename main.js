41bc967cfb56110dcc3a87c26b0778eb31bbe2ca
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2017-11-01 21:19:56 +0000

    numberlist is now calc array, stores numbers and operators

---
+++ main.js
@@ -0,0 +1,36 @@
+//click on number/operator buttons, logged into an array.
+
+var calcList = [];
+  function buttPress(value){
+  calcList.push(value);
+  document.getElementsByClassName("buttonNum").innerHTML = calcList.join('');
+}
+
+var calcList = [];
+  function operator(value){
+    calcList.push(value);
+    document.getElementsByClassName("operator").innerHTML = calcList.join('');
+}
+
+
+
+// var clearKeys = numberList.length = 0;
+
+
+//appear on screen
+
+//store in empty array
+
+//click operator number
+
+//appear on screen
+
+//store in same empty array
+
+//repeat if nessarcary
+
+//apply equals to close array/join array
+
+//reveal answer on screen
+
+//clear function to clear array
