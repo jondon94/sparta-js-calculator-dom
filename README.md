2aaefe94190fe6a65dfe6a1072a1045e58f32209
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-11-02 08:54:10 +0000

    final comit nothing works, ill keep trying though

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sparta-js-calculator-dom
+
+## brief
+
+1) Git repo with Dev and Master branches
+2) Me and Joe added as contributors
+3) Finished README.md with brief, what went well, the challenges and list of what you delivered
+4) pull-request submitted by 9:00am tomorrow
+5) The calculator should display the correct value for each button pressed in the browser
+6) when the equals button is pressed you should display the result of the last entered calculation
+7) the program should use functions, loops, DOM selectors and DOM events
+
+## result
+I didnt get anything donw really. None of it works the way it should.
+
+## Difficulties
+Yes
+
+I just need to pul my socks up and learn it more
--- main.js
+++ main.js
@@ -1,20 +1,60 @@
 //click on number/operator buttons, logged into an array.
+// function (value) {
+//   var value = button.value;
+//   document.getElementById("screen").innerHTML = value;
+// }
+var compute = []
 
-var calcList = [];
-  function buttPress(value){
-  calcList.push(value);
-  document.getElementsByClassName("buttonNum").innerHTML = calcList.join('');
-}
+  var calcList = compute;
+    function buttPress(value){
+      calcList.push(value);
+      document.getElementsByClassName("button").innerHTML = calcList.push();
+      return calcList;
+  }
 
-var calcList = [];
-  function operator(value){
-    calcList.push(value);
-    document.getElementsByClassName("operator").innerHTML = calcList.join('');
-}
+  var calcList = compute;
+    function buttPress(value){
+      calcList.push(value);
+      document.getElementsByClassName("operator").innerHTML = calcList.push();
+      return calcList;
+  }
 
 
 
-// var clearKeys = numberList.length = 0;
+
+
+
+// <------------------ ----------------->
+
+
+// function numbList(){
+//   var compLine = calcList.toString();
+//   return compLine;
+// }
+
+
+// var compLine = [];
+//   function calclist(){
+//     var s = Array.prototype.join.call(arguments);
+//     console.log(s);
+//   }
+
+// var compLine = []
+//   function compute(value){
+//   calcListNum.concat(calcListOp);
+//   return compLine;
+// }
+
+// function reverseWord(word) {
+//   return word.split("").reverse().join("");
+// }
+// <------------------------------- ---------------------------------->
+// var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+//     // "olleh"
+//
+//     //Step 4. Return the reversed string
+//     return joinArray; // "olleh
+// // var clearKeys = numberList.length = 0;
 
 
 //appear on screen
